33994f7dde6fadfd0912c0d257a65dc724520de7
Adewale Orotayo <didymus7007@gmail.com>
2021-03-04 08:53:42 +0100

    fixing calculate logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,33 @@
+/* eslint-disable radix */
+/* eslint-disable no-unused-expressions */
 const calculate = (data, buttonName) => {
-  const { operation } = data;
-  let { total, next } = data;
+  debugger
+  let { total, next, operation } = data;
+  let first = '';
+  let operations = ['/', '*', '-', '+', '%'];
+  // total = 0;
+  next = '';
+
+  const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  if (digits.includes(buttonName)) {
+    next += buttonName;
+    console.log(next);
+  }
+  if (buttonName === '.') {
+    if (next === '') {
+      next = `0${buttonName}`;
+      console.log(next);
+    }
+    if (!next.includes('.')) {
+      next + buttonName;
+      console.log(next);
+    }
+  }
+  // if (operations.includes(buttonName)) {
+  // }
+  next.includes('.') ? first = parseFloat(next) : first = parseInt(next);
+  return first;
 };
 
-export default calculate;
+console.log(calculate({ total: 0, next: '', operation: [] }, '52'));
+// export default calculate;

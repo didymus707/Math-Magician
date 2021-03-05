211cc9d575cfa1c6070bdc20b030dad3b4b8ec87
Adewale Orotayo <didymus7007@gmail.com>
2021-03-05 23:06:03 +0100

    Imported Button panel and set a default props for display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 import '../App.css';
 
 const App = () => (
   <div className="App">
-    <Display result="0" />
+    <Display />
+    <ButtonPanel />
   </div>
 );
 
--- src/components/Display.js
+++ src/components/Display.js
@@ -8,7 +8,11 @@ const Display = ({ result }) => (
 );
 
 Display.propTypes = {
-  result: PropTypes.string.isRequired,
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
 };
 
 export default Display;

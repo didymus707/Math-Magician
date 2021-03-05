047c3ac9b3bd197e8c1ce90716cd5980838848b9
Adewale Orotayo <didymus7007@gmail.com>
2021-03-05 21:56:13 +0100

    Calculate js can modify parameters based on arguments given

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,34 +1,44 @@
 /* eslint-disable radix */
 /* eslint-disable no-unused-expressions */
+import operate from './operate';
+
 const calculate = (data, btn) => {
   let { total, next, operation } = data;
-  total = '';
-  next = '';
+  total = null;
+  next = null;
   const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   const operators = ['/', 'X', '-', '+', '%'];
+  if (btn === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  }
   if (digits.includes(btn)) {
-    next += btn;
+    next ? next += btn : next = btn;
   }
   if (btn === '.') {
     if (!next.includes('.')) {
       next += btn;
     }
-    if (next === '') {
+    if (next === null) {
       next = `0${btn}`;
     }
   }
   if (operators.includes(btn)) {
     operation = btn;
-    total += next;
-    next = '';
+    total = next;
+    next = null;
   }
   if (btn === '+/-') {
     total.includes('.') ? total = (parseFloat(total) * -1).toString() : (parseInt(total) * -1).toString();
     next.includes('.') ? next = (parseFloat(next) * -1).toString() : (parseInt(next) * -1).toString();
   }
   if (btn === '=') {
-    total = eval(total, operation, next);
+    if (total && operation && next) { total = operate(total, next, operation); }
+    next = null;
+    operation = null;
   }
+  return {total, next, operation };
 };
 
 export default calculate;

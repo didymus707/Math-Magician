3644e56e2d5a3d3e2c181badb0422c0e3716cd15
Adewale Orotayo <didymus7007@gmail.com>
2021-03-05 23:50:37 +0100

    Converted functional app component to a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
+import React, { Component } from 'react'
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // import calculate from '../logic/calculate';
 import '../App.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    }
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
 
 const App = () => (
   <div className="App">

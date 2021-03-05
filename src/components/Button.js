173bb9ab4a2da95ae4e99801d3d95f2f1970d001
Adewale Orotayo <didymus7007@gmail.com>
2021-03-05 22:56:09 +0100

    Added Button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ name }) => (
+  <button type="button" className="btn">{name}</button>
+);
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Button;

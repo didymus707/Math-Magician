a08e5f34ff20f4198a23f191f3dea817ebcabac5
Adewale Orotayo <didymus7007@gmail.com>
2021-03-19 14:51:04 +0100

    Imported Calculator and Quote Used Route for each component Added Quote

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/calculator" component={Calculator} />
+      <Route exact path="/quote" component={Quote} />
     </Switch>
   </BrowserRouter>
 );
